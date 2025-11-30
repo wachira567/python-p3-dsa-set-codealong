8acad14ae28f1ced20245dbbf91c963b77bbe37f
+ <+>
2025-11-30 21:20:50 +0300

    add solution

--- lib/testing/set_test.py
+++ lib/testing/set_test.py
@@ -1,54 +1,51 @@
 #!/usr/bin/env python3
 from MySet import MySet
 
-class TestSet:
 
+class TestSet:
     def test_init(self):
-        '''Test __init__ set with list'''
-        test_set = MySet([1,2,3,4])
-        set_list = [1,2,3,4]
+        """Test __init__ set with list"""
+        test_set = MySet([1, 2, 3, 4])
+        set_list = [1, 2, 3, 4]
         for num in set_list:
-            assert(num in test_set.dictionary)
+            assert num in test_set.dictionary
 
     def test_add(self):
-        '''Test add() to set'''
-        test_set = MySet([1,2,3,4])
+        """Test add() to set"""
+        test_set = MySet([1, 2, 3, 4])
         test_set.add(5)
-        set_list = [1,2,3,4,5]
+        set_list = [1, 2, 3, 4, 5]
         for num in set_list:
-            assert(num in test_set.dictionary)
+            assert num in test_set.dictionary
 
     def test_delete(self):
-        '''Test delete()'''
-        test_set = MySet([1,2,3,4])
+        """Test delete()"""
+        test_set = MySet([1, 2, 3, 4])
         test_set.delete(2)
-        set_list = [1,3,4]
+        set_list = [1, 3, 4]
         for num in set_list:
-            assert(num in test_set.dictionary)
+            assert num in test_set.dictionary
 
     def test_has(self):
-        '''Test has()'''
-        test_set = MySet([1,2,3,4])
-        assert(test_set.has(1) == True)
-        assert(test_set.has(7) == False)
+        """Test has()"""
+        test_set = MySet([1, 2, 3, 4])
+        assert test_set.has(1) == True
+        assert test_set.has(7) == False
 
     def test_size(self):
-        '''Test size()'''
-        test_set = MySet([1,2,3,4])
-        assert(len(test_set.dictionary) == 4)
+        """Test size()"""
+        test_set = MySet([1, 2, 3, 4])
+        assert len(test_set.dictionary) == 4
 
+    # Bonus test
 
-
-# Bonus test
-"""
     def test_clear(self):
-        '''Test clearing set'''
-        test_set = MySet([1,2,3,4])
+        """Test clearing set"""
+        test_set = MySet([1, 2, 3, 4])
         test_set.clear()
-        assert(len(test_set.dictionary) == 0)
+        assert len(test_set.dictionary) == 0
 
     def test_str(self):
-        '''Test __str__()'''
-        test_set = MySet([1,2,3,4])
-        assert(str(test_set) == 'MySet: {1,2,3,4}')
-"""
+        """Test __str__()"""
+        test_set = MySet([1, 2, 3, 4])
+        assert str(test_set) == "MySet: {1,2,3,4}"
